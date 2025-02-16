d9d95d25b7667b34fd4abf89e9c1ae871fd95211
yeguoz <aidjajd@163.com>
2025-02-16 18:06:07 +0800

    refactor: 从localStorage获取个性化主题

--- src/store/useThemeStore.ts
+++ src/store/useThemeStore.ts
@@ -4,9 +4,13 @@ interface ThemeStore {
   isDark: boolean,
   setIsDark: (b: boolean) => void;
 }
+const getInitialTheme = () => {
+  const storedTheme = localStorage.getItem('isDark');
+  return storedTheme ? JSON.parse(storedTheme) : false;
+};
 
 const useThemeStore = create<ThemeStore>((set) => ({
-  isDark: false,
+  isDark: getInitialTheme(),
   setIsDark: (isDark) => set({ isDark }),
 }));
 
