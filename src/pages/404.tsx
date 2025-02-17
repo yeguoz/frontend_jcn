5cb34ab4d89c65ee4cbc543d405d58fbec8f1f9e
yeguoz <aidjajd@163.com>
2025-02-17 21:05:06 +0800

    refactor: 404page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,21 @@
-
-export const NotFound = () => {
-  return <div>404 Not Found</div>;
+import { FrownOutlined } from "@ant-design/icons";
+const NotFound = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        fontSize: "2rem",
+        textAlign: "center",
+        marginTop: "6rem",
+        color: "#6b798e",
+      }}
+    >
+      <div style={{marginBottom: "1rem"}}>
+        <FrownOutlined style={{fontSize: "10rem"}}/>
+      </div>
+      <div>404 Not Found</div>
+    </div>
+  );
 };
+
+export default NotFound;
