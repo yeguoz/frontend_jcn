4451b76bf2360af89c2cf1e55fa6f2833d97e24e
yeguoz <aidjajd@163.com>
2025-02-21 23:06:16 +0800

    feat: 路由守卫

---
+++ src/components/AuthGuard/index.tsx
@@ -0,0 +1,14 @@
+import useAuthGuard from "../../hooks/useAuthGuard";
+
+const AuthGuard = ({ children }: { children: React.ReactNode }) => {
+  
+  const redirect = useAuthGuard();
+
+  if (redirect) {
+    return redirect;
+  }
+
+  return children;
+};
+
+export default AuthGuard;
