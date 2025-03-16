ff02164bf45cd87fdfb8e90c62b38ae48276267c
yeguoz <aidjajd@163.com>
2025-03-16 12:38:12 +0800

    refactor: themeConfig

--- config/themeConfig.ts
+++ config/themeConfig.ts
@@ -3,17 +3,41 @@ import { theme } from "antd";
 const themeConfig = {
   algorithm: [theme.compactAlgorithm],
   token: {
-    colorPrimary: "#d77409",
-    colorLink: "#d77409",
     colorBgBase: "#fafafa",
-    borderRadius: 6,
     fontSize: 16,
+  }
+};
+
+const lightTheme = {
+  ...themeConfig,
+  token: {
+    ...themeConfig.token,
+    colorPrimary: "#C45100",
+    colorLink: "#C45100",
   },
   components: {
     Notification: {
-      width: "250px",
+      width: 250,
       algorithm: true,
+      colorBgBase: "#fff",
     },
+    Button: {
+      defaultHoverBg: "#fff"
+    },
+    Menu: {
+      itemHeight: 45,
+      iconSize: 16,
+      subMenuItemBg: "#fff",
+      itemSelectedBg: "#fff",
+      iconMarginInlineEnd: 15,
+    },
+    Table: {
+      headerBg: "#fafafa",
+      cellPaddingBlock: 10,
+      headerSortActiveBg: "#fafafa",
+      headerSplitColor: "none"
+    }
   }
 };
-export default themeConfig;
+
+export { lightTheme };
