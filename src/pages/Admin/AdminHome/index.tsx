d89a970dfad84224e523877fdfaee9c9c110f7ae
yeguoz <aidjajd@163.com>
2025-05-10 19:07:05 +0800

    chore: del

--- src/pages/Admin/AdminHome/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const AdminHome = () => {
-  return (
-    <div>
-      AdminHome
-    </div>
-  )
-}
--- src/pages/Admin/Basic/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Basic = () => {
-  return (
-    <div>Basic</div>
-  )
-}
