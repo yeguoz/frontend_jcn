8c40e99b0c7fab1b54c6fcd3107345f8f20b5f56
yeguoz <aidjajd@163.com>
2025-05-11 15:38:00 +0800

    refactor: Verify

--- src/pages/Auth/Verify/index.tsx
+++ src/pages/Auth/Verify/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useParams, useSearchParams } from "react-router";
 import { verifyEmail } from "../../../services/userController";
 
