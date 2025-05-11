2af4800ac8f599f59fe78d362f814de55bcd8ee3
yeguoz <aidjajd@163.com>
2025-05-11 14:42:41 +0800

    feat: MultipleMenu

--- src/components/contextMenu/MultipleMenu.tsx
+++ src/components/contextMenu/MultipleMenu.tsx
@@ -1,26 +1,54 @@
 import React from "react";
-import {
-  CloudDownloadOutlined,
-  DeleteOutlined,
-  ExportOutlined,
-} from "@ant-design/icons";
+import { DeleteOutlined, ExportOutlined } from "@ant-design/icons";
 import styles from "./index.module.css";
 import useVisibleRowsPosStore from "../../store/useVisibleRowsPosStore";
 import Item from "./Item";
-import ZipIcon from "../icon/ZipIcon";
 
 const MultipleMenu = React.forwardRef<
   HTMLDivElement,
   {
     style?: React.CSSProperties;
   }
->(({ style}, ref) => {
+>(({ style }, ref) => {
   const multipleMenuPosition = useVisibleRowsPosStore(
     (state) => state.multipleMenuPosition
+  );
+  const setMultipleMenuVisible = useVisibleRowsPosStore(
+    (state) => state.setMultipleMenuVisible
+  );
+  const setTreeModalVisible = useVisibleRowsPosStore(
+    (state) => state.setTreeModalVisible
+  );
+  const setTreeModalType = useVisibleRowsPosStore(
+    (state) => state.setTreeModalType
+  );
+  const setEditModalVisible = useVisibleRowsPosStore(
+    (state) => state.setEditModalVisible
+  );
+  const setEditModalType = useVisibleRowsPosStore(
+    (state) => state.setEditModalType
   );
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
+  };
+
+  const onMove = () => {
+    setMultipleMenuVisible?.(false);
+    setTreeModalVisible(true);
+    setTreeModalType("move");
+  };
+
+  const onCopy = () => {
+    setMultipleMenuVisible?.(false);
+    setTreeModalVisible(true);
+    setTreeModalType("copy");
+  };
+
+  const onDelete = () => {
+    setMultipleMenuVisible?.(false);
+    setEditModalVisible(true);
+    setEditModalType("delete");
   };
 
   return (
@@ -36,10 +64,9 @@ const MultipleMenu = React.forwardRef<
       onMouseMove={handleMouseMove}
     >
       <div className={styles.container}>
-        <Item icon={<CloudDownloadOutlined />} title={"打包下载"} />
-        <Item icon={<ZipIcon />} title={"压缩"} />
-        <Item icon={<ExportOutlined />} title={"移动"} />
-        <Item icon={<DeleteOutlined />} title={"删除"} />
+        <Item icon={<ExportOutlined />} title={"移动"} onClick={onMove} />
+        <Item icon={<ExportOutlined />} title={"复制"} onClick={onCopy} />
+        <Item icon={<DeleteOutlined />} title={"删除"} onClick={onDelete} />
       </div>
     </div>
   );
